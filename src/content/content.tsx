e3394578c6256a1105f3557548a994edf87970fa
Daniel Demelash <danielddemissie@gmail.com>
2023-08-23 10:20:29 +0300

    fix: remove separate warning page implementation

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,6 +1,7 @@
 //@ts-nocheck
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../event/store';
+import DangerousContentPage from './components/dangerous';
 import { getActiveTab, getSourceData, setSourceType } from './features/store/source/sourceSlice';
 import { defineIconName } from './utils/icon.util';
 import YoutubeContentPage from './components/youtube';
@@ -16,14 +17,6 @@ import { checkSourceType } from './features/store/source/actions';
 import { getOriginFromHref, removeWWW } from './utils/index.util';
 import { browser } from '../browser-service';
 import { selectIsAuth } from '../popup/features/store/auth';
-import MessageManagerComponent from './message-manager.component';
-
-const isSafeSuspect = (href: string) => {
-	const dangerousProtocol = ['javascript:'];
-	const suspectUrl = new URL(href);
-
-	return dangerousProtocol.indexOf(suspectUrl.protocol) < 0;
-};
 
 function ContentComponent() {
 	const { host, href, pathname, search } = document.location;
@@ -31,26 +24,25 @@ function ContentComponent() {
 	if (checkList.includes(cutHost) && (pathname.length > 1 || search)) {
 		cutHost = href;
 	}
-
+	const dispatch = useAppDispatch();
 	const sourceData = useAppSelector(getSourceData);
 	const activeTab = useAppSelector(getActiveTab);
 	const isAuth = useAppSelector(selectIsAuth);
 	const activeOrigin = getOriginFromHref(activeTab);
+
 	const handleCheckSourceType = (host: string) => {
-		// @ts-ignore
 		storeWithMiddleware.then(({ dispatch }) => dispatch(checkSourceType(host)));
 	};
 
 	useEffect(() => {
 		if (href === activeTab) {
+			// check scam for all user
 			handleCheckSourceType(cutHost);
 		}
 	}, [host, activeOrigin, isAuth]);
 
 	useEffect(() => {
-		if (href.includes(`${process.env.REACT_APP_NIGHTHAWK_WARNING}`)) {
-			browser?.runtime?.sendMessage({ newIconPath: '/assets/logo/ic-nighthawk-dangerous.png' }, (response) => response);
-		} else if (sourceData && sourceData[cutHost]) {
+		if (sourceData && sourceData[cutHost]) {
 			browser?.runtime?.sendMessage(
 				{ newIconPath: `/assets/logo/${defineIconName(sourceData[cutHost])}` },
 				(response) => response
@@ -60,14 +52,9 @@ function ContentComponent() {
 
 	return (
 		<>
-			{sourceData &&
-				sourceData[cutHost] === EWebStatus.DANGEROUS &&
-				isSafeSuspect(href) &&
-				window.location.replace(`${process.env.REACT_APP_NIGHTHAWK_WARNING}?url=${href}`)}
-
+			{sourceData && sourceData[cutHost] === EWebStatus.DANGEROUS && <DangerousContentPage />}
 			{host === HOST_KEYS.YOUTUBE && <YoutubeContentPage />}
 			{host === HOST_KEYS.NIGHTHAWK_WEB_VERSION && <TokenManagerComponent />}
-			{href.includes(new URL(`${process.env.REACT_APP_NIGHTHAWK_WARNING}`).host) && <MessageManagerComponent />}
 			{host === HOST_KEYS.TWITTER && <TwitterContentPage />}
 			{host === HOST_KEYS.GOOGLE && <GoogleContentPage />}
 			{host === HOST_KEYS.FACEBOOK && <FacebookContentPage />}
